d3c6dbf92d1deb174556520fe93627d4d0d1f520
EricJPogue <epogue@epogue.com>
2022-06-13 15:21:41 -0500

    Add ConnectionConstants.js.

---
+++ ConnectionConstants.js
@@ -0,0 +1,5 @@
+const uri = "mongodb+srv://test-2022-06-09-user:test-2022-06-09-password@cluster0.e32od17.mongodb.net/?retryWrites=true&w=majority"	
+const databaseName = "movieDatabase"
+const collectionName = "movieCollection"
+
+module.exports = { uri, databaseName, collectionName };
